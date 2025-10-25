901bf6e6b35f646133144ae5d2db1c5a52bd154e
Anish010 <anishmohanty101@gmail.com>
2025-10-25 23:42:15 +0530

    added auth-service project

---
+++ collabrix-auth-service/src/test/java/com/collabrix/auth/collabrix_auth_service/CollabrixAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabrix.auth.collabrix_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabrixAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
