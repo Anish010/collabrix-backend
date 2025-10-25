80dc3406ca54004250e7077f9a683aadba438e11
Anish010 <anishmohanty101@gmail.com>
2025-10-25 22:19:58 +0530

    Initial commit

---
+++ src/test/java/com/collabrix/CollabrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
